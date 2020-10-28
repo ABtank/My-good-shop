0909f92509cac658a7ed381dafbcffcf0953119c
Abramov Iurii <abtank@outlook.com>
2020-10-29 00:01:06 +0300

    Initial commit

---
+++ shop-ui-admin/src/test/java/ru/abtank/ShopUiAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.abtank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopUiAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
