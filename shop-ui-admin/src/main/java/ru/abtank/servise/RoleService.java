131450ff4e611b01dbb7b3c13c8a909f8fdc501c
Abramov Iurii <abtank@outlook.com>
2020-11-01 02:27:38 +0300

    create services and controllers users/roles

---
+++ shop-ui-admin/src/main/java/ru/abtank/servise/RoleService.java
@@ -0,0 +1,19 @@
+package ru.abtank.servise;
+
+import ru.abtank.representation.BrandRepr;
+import ru.abtank.representation.RoleRepr;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+public interface RoleService {
+
+    List<RoleRepr> findAll();
+
+    Optional<RoleRepr> findById(Long id);
+
+    void deleteById(Long id);
+
+    void save(RoleRepr roleRepr) throws IOException;
+}
