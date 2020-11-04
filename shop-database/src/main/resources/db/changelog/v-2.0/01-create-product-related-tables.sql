7422f69aa07b3cbbb0d22bc87fce9c3e611ba249
Abramov Iurii <abtank@outlook.com>
2020-11-04 03:24:25 +0300

    fix sql v-2.0

--- shop-database/src/main/resources/db/changelog/v-2.0/01-create-product-related-tables.sql
+++ shop-database/src/main/resources/db/changelog/v-2.0/01-create-product-related-tables.sql
@@ -7,7 +7,21 @@ GO
 
 CREATE TABLE categories
 (
-    id   bigserial PRIMARY KEY,
+    id   serial PRIMARY KEY,
+    name varchar(50) NOT NULL UNIQUE
+);
+GO
+
+CREATE TABLE statuses
+(
+    id   serial PRIMARY KEY,
+    name varchar(50) NOT NULL UNIQUE
+);
+GO
+
+CREATE TABLE type_of_product
+(
+    id   serial PRIMARY KEY,
     name varchar(50) NOT NULL UNIQUE
 );
 GO
@@ -15,12 +29,17 @@ GO
 CREATE TABLE products
 (
     id          bigserial PRIMARY KEY,
-    name        varchar(255) NOT NULL,
-    price       numeric(19, 2) DEFAULT NULL,
-    brand_id    bigint NOT NULL,
-    category_id bigint NOT NULL,
+    name        varchar(255)                                            NOT NULL,
+    discount    smallint CHECK (discount between 0 AND 100) DEFAULT (0),
+    price       numeric(19, 2)                              DEFAULT NULL,
+    category_id int                                                     NOT NULL,
+    brand_id    bigint                                                  NOT NULL,
+    type_id     int                                                     NOT NULL,
+    status_id   int                                         DEFAULT (1) NOT NULL,
     FOREIGN KEY (brand_id) REFERENCES brands (id),
-    FOREIGN KEY (category_id) REFERENCES categories (id)
+    FOREIGN KEY (category_id) REFERENCES categories (id),
+    FOREIGN KEY (status_id) REFERENCES statuses (id),
+    FOREIGN KEY (type_id) REFERENCES type_of_product (id)
 );
 GO
 
---
+++ shop-database/src/main/resources/db/changelog/v-2.0/01-insert-categories-brands-statuses-products-related-tables.sql
@@ -0,0 +1,38 @@
+INSERT INTO statuses (name)
+VALUES ('Хит'),
+       ('Новинка'),
+       ('Распродажа'),
+       ('Акция'),
+       ('Уценка'),
+       ('Рассрочка');
+GO
+
+INSERT INTO brands (name)
+VALUES ('Roadstar'),
+       ('Levis'),
+       ('Persol'),
+       ('Nike'),
+       ('Edwin'),
+       ('New Balance'),
+       ('Paul Smith'),
+       ('Ray-Ban');
+GO
+
+INSERT INTO categories (name)
+VALUES ('Men'),
+       ('Women'),
+       ('Kids'),
+       ('Accessories'),
+       ('Shoes'),
+       ('Guns');
+GO
+
+INSERT INTO type_of_product (name)
+VALUES ('Sofa Cum Beds'),
+       ('Bags'),
+       ('Caps & Hats'),
+       ('Jackets & Coats'),
+       ('Jeans'),
+       ('Shirts'),
+       ('Sunglasses'),
+       ('Swimwear');
